da1be7f44507fc7be64b60d7a421fc98b6651d48
DikshantJangra <dikshantjangra1@gmail.com>
2025-03-24 03:23:48 +0530

    Attack Base

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Navbar from './components/Navbar'
+import Landing from './components/Landing'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='h-screen w-full'>
+      <Navbar />
+      <Landing />
+    </div>
   )
 }
 
