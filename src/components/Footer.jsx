331b0c54798df6b75b797d7d8678865cb5f390ae
DikshantJangra <dikshantjangra1@gmail.com>
2025-04-10 23:14:52 +0530

    Responsive Footer attacked ⚔

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,23 +3,32 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-        <div className='relative grid grid-cols-2'>
-            <div className='h-full'>
-                <img src="/footerImg.png" alt="AOT team" />
+        <div className='relative grid sm:grid-cols-2 h-[33vmax]'>
+            <div className='h-full bg-[url(/footerImg.png)] bg-cover bg-right hidden sm:block'>
             </div>
-            <div className='h-full text-white text-right pr-7'>
-                <h1 className='uppercase font-bold text-[5vmax]'>Attack On Titan</h1>
-                <div className='text-left pl-24'>
-                    <ul>
-                        <li>Home</li>
-                        <li>Episodes</li>
-                        <li>Characters</li>
-                        <li>Community</li>
-                        <li>Shop</li>
-                    </ul>
+            <div className='flex flex-col justify-between h-full text-white sm:pr-15'>
+                <div>
+                    <h1 className='uppercase font-bold text-[5vmax] text-center sm:text-left leading-none'>Attack On Titan</h1>
+                    <div className='sm:flex justify-between text-center sm:text-left pt-7'>
+                        <ul className='text-lg flex justify-center sm:block gap-3'>
+                            <p>Home</p>
+                            <p>Episodes</p>
+                            <p>Characters</p>
+                            <p>Community</p>
+                            <p>Shop</p>
+                        </ul>
+                        <ul>
+                            <li className='font-bold'>attackontitan@aot.com</li>
+                        </ul>
+                    </div>
+                </div>
+                <div className='text-center py-2'>
+                    <p className='py-1'>Privacy Policy | Terms of Service | Cookie Policy</p>
+                    <p className='font-bold text-sm'>© 2025 Attack on Titan. All rights reserved.</p>
                 </div>
             </div>
         </div>
+        
     </div>
   )
 }
